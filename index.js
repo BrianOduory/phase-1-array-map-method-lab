d5978fd13cb02ba05057dc5297a5dc9538101983
Brian Oduory <brianmubweka17@gmail.com>
2025-04-17 23:04:52 +0300

    creates index.js

--- index.js
+++ index.js
@@ -14,3 +14,16 @@ const tutorials = [
 const titleCased = () => {
   return tutorials
 }
+
+function titleCased() {
+  return tutorials.map(tutorial => {
+    const words = tutorial.split(' ');
+
+    const titleCasedWords = words.map(word => {
+      if (word.length === 0) return word;
+      return word[0].toUpperCase() + word.slice(1).toLowerCase();
+    });
+    
+    return titleCasedWords.join(' ');
+  });
+}
